11db65a5179b46ce21115cd0b86e75e257c93913
adamilhamadn <adamilhamadn@users.noreply.github.com>
2026-08-04 20:54:37 +0800

    fix: wrap root layout in GestureHandlerRootView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
 import '../global.css';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ThemeProvider } from '../theme';
 
 export default function Layout() {
   return (
-    <ThemeProvider>
-      <StatusBar style="light" />
-      <Stack screenOptions={{ headerShown: false }} />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider>
+        <StatusBar style="light" />
+        <Stack screenOptions={{ headerShown: false }} />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
